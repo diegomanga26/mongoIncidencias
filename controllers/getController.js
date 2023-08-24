304724baaaf8992f77f82a923372cc818e97c45d
diegomanga26 <diegoalexandermanga@gmail.com>
2023-08-24 17:47:06 -0500

    feat: :sparkles: Añadiendo funciones en controladores.
    Se añaden funciones para manipular la data de entrada a las funciones para las peticiones en Incidencias.

--- controllers/getController.js
+++ controllers/getController.js
@@ -1,5 +1,13 @@
 import * as getService from '../services/getService.js';
 
+/**
+ * ? TRAINERS:
+ */
+
+/**
+ * ! CONTROLADOR PARA TRAER TODOS LOS TRAINERS:
+ */
+
 export const getTrainersController = async (req, res) => {
     try {
         const result = await getService.getTrainersService();
@@ -10,6 +18,10 @@ export const getTrainersController = async (req, res) => {
     }
 }
 
+/**
+ * ! CONTROLADOR PARA TRAER TRAINER POR "id":
+ */
+
 export const getTrainerController = async (req, res) => {
     try {
         const { id } = req.query;
@@ -19,5 +31,39 @@ export const getTrainerController = async (req, res) => {
         console.log(error);
         res.status(500).send(error);
         
+    }
+}
+
+/**
+ * ? INCIDENCIAS:
+ */
+
+/**
+ * ! CONTROLADOR PARA TRAER TODAS LAS INCIDENCIAS:
+ */
+
+export const getIncidenciasController = async (req, res) => {
+    try {
+        const result = await getService.getIncidenciasService();
+        res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
+/**
+ * ! CONTROLADOR PARA TRAER INCIDENCIA POR "id":
+ */
+
+export const getIncidenciaController = async (req, res) => {
+    try {
+        const { id } = req.query;
+        const result = await getService.getIncidenciaService(parseInt(id));
+        res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+
     }
 }
