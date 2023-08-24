91894de77178746cc3f7dbbdb4e23e1376c64c8e
diegomanga26 <diegoalexandermanga@gmail.com>
2023-08-24 17:42:30 -0500

    feat: :sparkles: Se añaden servicios.
    Se añaden servicios segun colección.

--- services/getService.js
+++ services/getService.js
@@ -1,4 +1,9 @@
 import { trainer } from "../collections/trainer.js";
+import { incidencia } from "../collections/incidencia.js"
+
+/**
+ * ! SERVICIOS DE "trainer":
+ */
 
 export const getTrainersService = async () => {
     const trainers = new trainer;
@@ -6,6 +11,18 @@ export const getTrainersService = async () => {
 };
 export const getTrainerService = async (id) => {
     const trainerID = new trainer;
-    
     return await trainerID.getTrainer(id);
+};
+
+/**
+ * ! SERVICIOS DE "incidencia"
+ */
+
+export const getIncidenciasService = async () => {
+    const incidencias = new incidencia;
+    return await incidencias.getIncidencias();
+};
+export const getIncidenciaService = async (id) => {
+    const incidenciaID = new incidencia;
+    return await incidenciaID.getIncidencia(id);
 };
