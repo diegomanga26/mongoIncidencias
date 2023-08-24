2480f8927f31c0f99a1fe06970a8065777e5752c
diegomanga26 <diegoalexandermanga@gmail.com>
2023-08-24 17:48:31 -0500

    feat: :poop: Añadiendo endpoints.
    Se añaden endpoints para el delete funcional y token que aun está en desarrollo.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import getInit from './getRoute.js';
+import deleteInit from './deleteRoute.js';
+import { tokenJWT } from '../middlewares/mwJWT.js';
 
 const initApi = () => {
     const apiRouter = Router();
     apiRouter.use('/get', getInit());
+    apiRouter.use('/token', tokenJWT);
+    apiRouter.use('/delete', deleteInit());
     return apiRouter;
 }
 
